cd03e27ff92a3692f63896aa723d3475f9dbd44a
Tyler Weng <tylercweng@gmail.com>
2018-05-28 13:55:47 -0700

    Add DOM

--- build.sbt
+++ build.sbt
@@ -4,6 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
+// plugins
 enablePlugins(ScalaJSPlugin)
 
+// use main module for scala-js
 scalaJSUseMainModuleInitializer := true
+
+// dependencies
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.6"
--- src/main/scala/hello/HelloWorld.scala
+++ src/main/scala/hello/HelloWorld.scala
@@ -1,7 +1,16 @@
 package hello
+import org.scalajs.dom.{document, Node}
 
 object HelloWorld {
+
+  def appendHello(targetNode: Node, text: String): Unit = {
+    val titleNode = document.createElement("h1")
+    val textNode = document.createTextNode(text)
+    titleNode.appendChild(textNode)
+    targetNode.appendChild(titleNode)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    appendHello(document.body, "Hello World")
   }
 }
